bbc51216564f51199f1ffc8d67fbbd8e71c48540
Paco Peng <pacopeng@Pacos-MacBook-Pro.local>
2023-05-14 18:59:12 +0800

    9090

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello Paco OpenShift for Developers!"
+		response = "Hello InfoSys OpenShift for Developers!"
 	}
 
 	fmt.Fprintln(w, response)
@@ -28,7 +28,7 @@ func main() {
 	http.HandleFunc("/", helloHandler)
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
-		port = "8080"
+		port = "9090"
 	}
 	go listenAndServe(port)
 
