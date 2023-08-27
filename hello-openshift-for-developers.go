e0f93de2d6fbd40e96b86f4b66f560248ba4aa80
pacopeng <zhipeng@redhat.com>
2023-08-28 02:02:52 +0800

    app commit

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hel OpenShift for Developers!"
+		response = "Hell0 OpenShift for Developers!"
 	}
 
 	fmt.Fprintln(w, response)
