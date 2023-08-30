c63024b3a8946c3078a6911768ba1495d59aa758
pacopeng <zhipeng@redhat.com>
2023-08-30 13:19:46 +0800

    update 4

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello-3 OpenShift for Developers!"
+		response = "Hello-5 OpenShift for Developers!"
 	}
 
 	fmt.Fprintln(w, response)
